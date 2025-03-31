cfbed36734c73fdd73f642c4b8d44fabaad6e366
Kevin <micorreo@queteimporta.com>
2025-03-31 02:28:37 -0300

    primer commit

---
+++ src/main/java/cl/ms/envios/internacional/EnviosInternacionalApplication.java
@@ -0,0 +1,13 @@
+package cl.ms.envios.internacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviosInternacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviosInternacionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/ms/envios/internacional/EnviosInternacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ms.envios.internacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviosInternacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
